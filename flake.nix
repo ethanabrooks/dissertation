08d889ebf7884bd090cd128b8faf89b16b38713d
ethanabrooks <ethanabrooks@gmail.com>
2023-12-20 16:23:28 -0500

    strip out everything except typst

--- flake.nix
+++ flake.nix
@@ -12,33 +12,15 @@
     out = system: let
       pkgs = import nixpkgs {
         inherit system;
-        config.allowBroken = true;
       };
-      inherit (pkgs) mkShell poetry2nix python39;
-      inherit (poetry2nix) mkPoetryApplication mkPoetryEnv;
-      python = python39;
-      overrides = pyfinal: pyprev: let
-        inherit (pyprev) buildPythonPackage fetchPypi;
-      in rec {};
-      poetryArgs = {
-        inherit python;
-        projectDir = ./.;
-        overrides = poetry2nix.overrides.withDefaults overrides;
-      };
-      poetryEnv = mkPoetryEnv poetryArgs;
       buildInputs = with pkgs; [
-        # chromedriver
-        # poetry
-        # poetryEnv
-        texlive.combined.scheme-full
         typst
         typst-lsp
         typstfmt
         typst-preview
-        # wget
       ];
     in rec {
-      devShell = mkShell rec {
+      devShell = pkgs.mkShell rec {
         inherit buildInputs;
       };
     };
