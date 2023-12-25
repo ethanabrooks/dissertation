0de70a7bc10b698da20d9afdaa3f1f8b807c8795
ethanabrooks <ethanabrooks@gmail.com>
2023-12-25 10:41:37 -0500

    add poetry2nix to inputs

--- flake.nix
+++ flake.nix
@@ -2,16 +2,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
+    poetry2nix,
   }: let
     out = system: let
       pkgs = import nixpkgs {
         inherit system;
+      };
+      inherit (pkgs) python311;
+      poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+        inherit python311;
+        projectDir = ./.;
+        preferWheels = true;
+        # overrides = poetry2nix.overrides.withDefaults overrides;
       };
       my-typst-preview = import ./pkgs/typst-preview/package.nix {
         inherit (pkgs) lib rustPlatform fetchFromGitHub mkYarnPackage fetchYarnDeps pkg-config libgit2 openssl zlib stdenv darwin;
@@ -26,6 +35,7 @@
         typstfmt
         typst-lsp
         wget
+        # poetryEnv
       ];
     in rec {
       devShell = pkgs.mkShell rec {
